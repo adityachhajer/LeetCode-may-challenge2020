f9af5d4b286e73551aaee2b3175adae5a44df680
aditya chhajer <chhajeraditya@gmail.com>
2020-05-15 12:33:02 +0530

    Add files via upload

---
+++ day14_ImplementTriePrefixTree.py
@@ -0,0 +1,66 @@
+class TrieNode(object):
+    def __init__(self):
+
+        self.isEnd = False
+        self.children = dict()
+
+
+class Trie(object):
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+        currNode = self.root
+
+        for c in word:
+            if c not in currNode.children:
+                currNode.children[c] = TrieNode()
+
+            currNode = currNode.children[c]
+
+        currNode.isEnd = True
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        currNode = self.root
+
+        for c in word:
+            if c not in currNode.children:
+                return False
+
+            currNode = currNode.children[c]
+
+        return currNode.isEnd
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie
+        that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        currNode = self.root
+
+        for c in prefix:
+            if c not in currNode.children:
+                return False
+
+            currNode = currNode.children[c]
+
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
