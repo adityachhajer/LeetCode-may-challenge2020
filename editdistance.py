7a921455206348d39af149a714826894564f14da
aditya chhajer <chhajeraditya@gmail.com>
2020-06-01 00:49:21 +0530

    Add files via upload

---
+++ editdistance.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        if len(word1) == 0:
+            return len(word2)
+        if len(word2) == 0:
+            return len(word1)
+        dp = [[0 for _ in range(len(word2) + 1)] for _ in range(len(word1) + 1)]
+        for i in range(len(word1) + 1):
+            for j in range(len(word2) + 1):
+                if i == 0:
+                    dp[0][j] = j
+                elif j == 0:
+                    dp[i][0] = i
+                elif word1[i - 1] == word2[j - 1]:
+                    dp[i][j] += dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = 1 + min(dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j])
+        return dp[len(word1)][len(word2)]
+
+
